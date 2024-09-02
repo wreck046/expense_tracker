89f7279d152a1446fb808bfdeb7b927766d51d65
wreck046 <ivankoroh46@gmail.com>
2024-09-02 15:03:19 +0800

    fixing some misplace and obscure items

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -28,7 +28,15 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpense() {
     showModalBottomSheet(
-        context: context, builder: (ctx) => const NewExpense());
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) => NewExpense(onAddExpense: _addExpense));
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
