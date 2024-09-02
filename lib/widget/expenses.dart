68c47a62db572fe6c4c61ed683ebe70b3e37c920
wreck046 <ivankoroh46@gmail.com>
2024-09-02 14:31:58 +0800

    adding a date picker and making a form for user to entry another child of expenses list.

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -25,9 +26,20 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpense() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(onPressed: _openAddExpense, icon: const Icon(Icons.add))
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
